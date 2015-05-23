bc7378f4037589947ba27e3602324f8f229e730c
Alex Burner <alex.burner@gmail.com>
2015-05-22 18:00:43 -0700

    adding css to gulp dev

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@ var path = require('path'),
     del = require('del'),
     nodemon = require('gulp-nodemon'),
     mincss = require('gulp-minify-css'),
-    rename = require("gulp-rename"),
-    clone = require("gulp-clone"),
-    order = require("gulp-order"),
+    rename = require('gulp-rename'),
+    clone = require('gulp-clone'),
+    order = require('gulp-order'),
     debug = require('gulp-debug'),
     sourcemaps = require('gulp-sourcemaps'),
     es = require('event-stream');
@@ -16,7 +16,12 @@ var path = require('path'),
 gulp.task('build-client-css', function() {
 
     var allCSS = gulp.src([
-        path.join(__dirname, 'client', 'css', 'style.css')
+        path.join(
+            __dirname,
+            'client',
+            'css',
+            'style.css'
+        )
     ])
         .pipe(concat('app.css'));
 
@@ -26,7 +31,14 @@ gulp.task('build-client-css', function() {
         .pipe(sourcemaps.write())
         .pipe(rename('app.min.css'));
 
-    return es.merge(allCSS, minCSS).pipe(gulp.dest(path.join(__dirname, 'client', 'build')));
+    return es.merge(allCSS, minCSS)
+        .pipe(gulp.dest(
+            path.join(
+                __dirname,
+                'client',
+                'build'
+            )
+        ));
 });
 
 gulp.task('build-client-js', function(){
@@ -34,36 +46,43 @@ gulp.task('build-client-js', function(){
     // write all files, in order to a concat file
     var allJS = gulp.src(path.join(__dirname, 'client', 'js', '**', '*.js'))
         .pipe(order([
-            "d3/*.js",
-            "angular/wikitree.module.js",
-            "angular/wikitree.routes.js",
-            "angular/services/*.js",
-            "angular/search/*.module.js",
-            "angular/search/*.js",
-            "angular/home/*.module.js",
-            "angular/home/*.js",
-            "angular/main/*.module.js",
-            "angular/main/*.js",
-            "angular/main/menu/*.module.js",
-            "angular/main/menu/*.js",
-            "angular/main/menu/session/*.module.js",
-            "angular/main/menu/session/*.js",
-            "angular/main/reader/*.module.js",
-            "angular/main/reader/*.js",
-            "angular/main/graph/*.module.js",
-            "angular/main/graph/*.js",
-            "angular/main/resizer/*.module.js",
-            "angular/main/resizer/*.js"
+            'd3/*.js',
+            'angular/wikitree.module.js',
+            'angular/wikitree.routes.js',
+            'angular/services/*.js',
+            'angular/search/*.module.js',
+            'angular/search/*.js',
+            'angular/home/*.module.js',
+            'angular/home/*.js',
+            'angular/main/*.module.js',
+            'angular/main/*.js',
+            'angular/main/menu/*.module.js',
+            'angular/main/menu/*.js',
+            'angular/main/menu/session/*.module.js',
+            'angular/main/menu/session/*.js',
+            'angular/main/reader/*.module.js',
+            'angular/main/reader/*.js',
+            'angular/main/graph/*.module.js',
+            'angular/main/graph/*.js',
+            'angular/main/resizer/*.module.js',
+            'angular/main/resizer/*.js'
         ]))
-        .pipe(concat("app.js"));
+        .pipe(concat('app.js'));
 
     // copy the concat file, uglify and rename
     var minJS = allJS.pipe(clone())
         .pipe(uglify())
-        .pipe(rename("app.min.js"));
+        .pipe(rename('app.min.js'));
 
     // merge the two files to one dest
-    return es.merge(allJS, minJS).pipe(gulp.dest(path.join(__dirname, 'client', 'build')));
+    return es.merge(allJS, minJS)
+        .pipe(gulp.dest(
+            path.join(
+                __dirname,
+                'client',
+                'build'
+            )
+        ));
 
 });
 
@@ -78,19 +97,41 @@ gulp.task('clean',function(cb) {
 gulp.task('dev', function() {
     // runs nodemon on the server file and executed the default task
     nodemon({
-        script: path.join(__dirname, 'server', 'server.js'),
+        script: path.join(
+            __dirname,
+            'server',
+            'server.js'
+        ),
         ext: 'js',
         ignore: ['node_modules/**']
     }).on('restart', ['default']);
 
-    gulp.start("default");
+    gulp.start('default');
 });
 
 gulp.task('default', ['clean'], function(){
-    gulp.start("build");
-    watch(path.join(__dirname, 'client', 'js', '**', '*.js'), function() {
+
+    gulp.start('build');
+
+    watch(path.join(
+        __dirname,
+        'client',
+        'js',
+        '**',
+        '*.js'
+    ), function() {
         gulp.start('build-client-js');
     });
+
+    watch(path.join(
+        __dirname,
+        'client',
+        'css',
+        'style.css'
+    ), function() {
+        gulp.start('build-client-css');
+    });
+
 });
 
 // ** stub for running unit & integration tests
@@ -100,9 +141,9 @@ gulp.task('test', function(){
 
 // ** commented out in case heroku is used at any time (using a gulp buildpack)
 //gulp.task('heroku:production', function() {
-//    gulp.start("build");
+//    gulp.start('build');
 //});
 //
 //gulp.task('heroku:development', function() {
-//    gulp.start("build");
+//    gulp.start('build');
 //});
