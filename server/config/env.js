0aaa3ae9fc92055645c77aef612043c1a90dae1a
Alex Burner <alex.burner@gmail.com>
2015-06-10 01:10:09 -0700

    hotfixing new env directory

--- server/config/env.js
+++ server/config/env.js
@@ -1,13 +1,23 @@
 'use strict';
 
+var path = require('path');
+
+var logger = require('../lib/log');
+
 /**
  * Load app configurations by environment
  */
-
-var logger = require('../lib/log');
 
 var env = process.env.NODE_ENV || 'local';
 
 logger.info('Environment = "' + env + '"');
 
-module.exports = require(__dirname + '../../../../wikitree-env/' + env);
+module.exports = require(
+	path.resolve(
+		__dirname,
+		'..',
+		'..',
+		'..',
+		'wikitree-env/' + env
+	)
+);
